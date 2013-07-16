197442f2a6793683f079a1e4cacd67e31715b86a
Bartuz <iamnotawowplayer@gmail.com>
2013-07-16 14:42:21 -0400

    Added assos.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ActiveRecord::Base
-  belongs_to :category
+   belongs_to :category
+
+  has_many :users, through: :roles
+
+  has_many :roles
+
+
+  scope :owner, where("#{self.users}.where(role: 'owner')")
+
   attr_accessible :chairs, :location, :menu, :name, :summary
 end
