4d4dd2663b11f73b8610f32a2d9d36ab5eb01b20
Bartuz <iamnotawowplayer@gmail.com>
2013-07-16 13:40:16 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Open::Application.config.secret_token = '0b0dd48e3f0fa91667469b3c8e1e990e961ba3508d84598bd2bb125bcf9bea20685ef62e0b3ac8f6301f27c336b762a7e98db0f10d447f617706a24fef00b1ec'
